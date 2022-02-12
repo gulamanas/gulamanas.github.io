3b4a198c5e3b7750b10b9e832c340bf4f1aa0cd9
Gulam Anas <g.anas786@gmail.com>
2022-02-12 18:53:58 +0530

    changed all files to .php

---
+++ _footer.php
@@ -0,0 +1,14 @@
+<!-- Socials -->
+<div class="socials">
+    <a target="_blank" href="https://www.facebook.com/gulamanas/"><i class="fab fa-facebook-f"></i></a>
+    <a target="_blank" href="https://www.instagram.com/gulamanas/"><i class="fab fa-instagram"></i></a>
+    <a target="_blank" href="https://twitter.com/_gulam_anas_"><i class="fab fa-twitter"></i></a>
+    <a target="_blank" href="https://www.linkedin.com/in/gulam-anas-79a0b01a1/"><i class="fab fa-linkedin"></i></a>
+</div>
+<!-- Socials -->
+
+<!-- Copyright -->
+<div class="copyright">
+    <p>Made with ❤️ by Gulam Anas Copyright &copy; 2022</p>
+</div>
+<!-- Copyright -->
